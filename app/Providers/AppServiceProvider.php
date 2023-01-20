e33d78c1396d87ad73067cb6edb7a24477070b50
mohammadTaimoorHussain <taimoor.hussain@olivedigital.com>
2023-01-21 03:46:35 +0500

    timezone modified by user role

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,23 +25,20 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        view()->composer('*', function ($view)
-        {
-            if(Auth::check()){
-                if(Auth::user()->role_id == 2){
-                    $user = \App\Models\Customer::where('user_id', Auth()->id())                  
-                    ->first(['first_name','last_name','photo']);                    
-                              
-                    $view->with('user', $user );
+        view()->composer('*', function ($view) {
+            if (Auth::check()) {
+                $user = null;
+                if (Auth::user()->role_id == 2) {
+                    $user = \App\Models\Customer::where('user_id', Auth()->id())
+                        ->first(['first_name', 'last_name', 'photo']);
+                } elseif (Auth::user()->role_id == 3) {
+                    $user = \App\Models\Trainer::where('user_id', Auth()->id())
+                        ->first(['name', 'last_name', 'photo']);
                 }
-                if(Auth::user()->role_id == 3){
-                    $user = \App\Models\Trainer::where('user_id', Auth()->id())                  
-                    ->first(['name','last_name','photo']);                    
-                              
-                    $view->with('user', $user );
-                }
+                $view->with('user', $user);
+                // update the timezones as per the user location
+                config(['app.timezone' => $user->time_zone]);
             }
-
         });
         Schema::defaultStringLength(191);
     }
