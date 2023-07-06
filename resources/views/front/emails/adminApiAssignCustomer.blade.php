b18a15055baa86ff7411cfc9eb04ad1f0bae4362
Sabih <sheikh.muhammad@olivedigital.com>
2023-07-07 00:08:08 +0500

    IBC

--- resources/views/front/emails/adminApiAssignCustomer.blade.php
+++ resources/views/front/emails/adminApiAssignCustomer.blade.php
@@ -1,15 +1,34 @@
+{{--@component('mail::message')--}}
+
+{{--    # Dear {{ $name }},--}}
+
+
+{{--# This email is to inform you that you are assigned to trainer for training. Trainer name is {{ $trainer }}..--}}
+
+{{--Start Date: {{ $start_date }}--}}
+{{--Start Time: {{ $start_time }}--}}
+
+{{--@component('mail::button', ['url' => $start_url])--}}
+{{--    Click Here to start the meeting--}}
+{{--@endcomponent--}}
+
+{{--Thanks,<br>--}}
+{{--{{ config('app.name') }}--}}
+{{--@endcomponent--}}
+
 @component('mail::message')
-# Dear {{ $name }},
+    # Dear {{ $customerData['name'] }},
 
-# This email is to inform you that you are assigned to trainer for training. Trainer name is {{ $trainer }}..
+    # This email is to inform you that you are assigned to trainer for training. Trainer name is {{ $customerData['trainer'] }}..
 
-Start Date: {{ $start_date }}
-Start Time: {{ $start_time }}
+    Start Date: {{ $customerData['start_date'] }}
+    Start Time: {{ $customerData['start_time'] }}
 
-@component('mail::button', ['url' => $start_url])
-    Click Here to start the meeting
+    @component('mail::button', ['url' => $customerData['start_url']])
+        Click Here to start the meeting
+    @endcomponent
+
+    Thanks,<br>
+    {{ config('app.name') }}
 @endcomponent
 
-Thanks,<br>
-{{ config('app.name') }}
-@endcomponent
