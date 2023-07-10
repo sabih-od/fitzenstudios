cdbf4bf100741ee91c118f59747c77d4058b5585
shahzaibkhan <shahzaibkhan@olivedigital.com>
2023-07-10 17:41:19 +0500

    fixed contactuscontroller and usercontroller api error

--- app/Http/Controllers/Api/ContactUsController.php
+++ app/Http/Controllers/Api/ContactUsController.php
@@ -8,42 +8,61 @@ use App\Traits\PHPCustomMail;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Validator;
+
 class ContactUsController extends Controller
 {
     use PHPCustomMail;
-       public  function contactUs(Request $request){
-           $validate = Validator::make($request->all(),[
-               'first_name'=>'required',
-               'last_name'=>'required',
-               'phone'=>'required',
-               'email'=>'required',
-               'message'=>'required',
-           ]);
-
-           if($validate->fails()){
-               return response()->json(['validate_error'=>$validate->messages(),]);
-
-           }else{
-               $name     = $request->first_name.' '.$request->last_name;
-               $mailData = array(
-                   'name'        => $name,
-                   'email'       => $request->email,
-                   'userMessage' => $request->message,
-                   'phone'       => $request->phone,
-                   'to'          => "info@fitzen.studio",
-               );
-               Mail::send('front.emails.contact-us', $mailData, function($message) use($mailData){
-                   $message->to($mailData['to'])->subject('Fitzen Studio - Contact Us');
-               });
-           }
-           return response()->json(['status'=> 200, 'message'=>'Your inquiry has been submitted successfully. We will contact you shortly!']);
-       }
 
 
+    public function contactUs(Request $request)
+    {
 
-         public function fAQs(){
-         $faqs = FAQCMS::all();
-        return response()->json(['status'=> 200, 'faqs'=>$faqs,]);
+        $validate = Validator::make($request->all(), [
+            'first_name' => 'required',
+            'last_name' => 'required',
+            'phone' => 'required',
+            'email' => 'required',
+            'message' => 'required',
+        ]);
+
+        if ($validate->fails()) {
+            return response()->json(['validate_error' => $validate->messages()]);
+        } else {
+            $name = $request->first_name . ' ' . $request->last_name;
+            $mailData = [
+                'name' => $name,
+                'email' => $request->email,
+                'userMessage' => $request->message,
+                'phone' => $request->phone,
+                'from' => "info@fitzen.studio",
+                'to' => $request->email,
+
+            ];
+
+            try {
+                Mail::send('front.emails.contact-us', $mailData, function ($message) use ($mailData) {
+                    $message->to($mailData['to'])->subject('Fitzen Studio - Contact Us');
+                });
+            } catch (\Exception $e) {
+                return response()->json([
+                    'status' => 500,
+                    'message' => 'An error occurred while sending the email. Please try again later.',
+                    'error' => $e->getMessage(),
+                ]);
+            }
+        }
+
+        return response()->json([
+            'status' => 200,
+            'message' => 'Your inquiry has been submitted successfully. We will contact you shortly!',
+        ]);
+    }
+
+
+    public function fAQs()
+    {
+        $faqs = FAQCMS::all();
+        return response()->json(['status' => 200, 'faqs' => $faqs,]);
     }
 
 }
